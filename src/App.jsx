5e3256e8a0d3db5181acdb2c1fd6caea24a7b0ab
denimozh <denimozh@gmail.com>
2024-02-01 22:42:11 +0000

    Restarting the Portfolio - it was too basic

--- src/App.jsx
+++ src/App.jsx
@@ -9,26 +9,9 @@ import AnimatedCursor from 'react-animated-cursor'
 import Slider from './Components/Slider'
 
 const App = () => {
-    useEffect(() => {
-        const script = document.createElement('script');
-        script.src = "/scripts/cursors.js";
-        script.async = true;
-        document.body.appendChild(script);
-      return () => {
-          document.body.removeChild(script);
-        }
-      }, []);
-
+    
   return (
     <div>
-        <AnimatedCursor innerSize={14}
-      outerSize={200}
-      color='255, 255, 255'
-      outerAlpha={0.2}
-      innerScale={0.7}
-      outerScale={2} outerStyle={{
-        mixBlendMode: 'exclusion'
-      }} trailingSpeed={10}	/>
         <div className=' bg-black text-white'>
             <Navbar/>
             <Hero/>
@@ -37,7 +20,6 @@ const App = () => {
             <About/>
             <Contact/>
             <Footer/>
-
         </div>
     </div>
   )
